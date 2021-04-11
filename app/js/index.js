8347bddedcb682044ab6454ac304a41ea0e7536c
Nikita <dedsec1612@gmail.com>
2021-04-11 14:47:23 +0300

    правки и часть JS

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,15 @@
-'Use strict'
-console.log('Hello! Test JS is here!')
+'Use scrict'
+
+let menuBurger = document.querySelector('.header__burder-button');
+let filterButton = document.querySelector('.filters__filter-button');
+let popupFilter = document.querySelector('.popup-filter');
+
+filterButton.onclick = function() {
+  popupFilter.classList.toggle('popup-filter--active');
+
+  if (popupFilter.classList.contains('header--active')) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+}
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,17 @@
 "use strict";
-'Use strict';
+'Use scrict';
 
-console.log('Hello! Test JS is here!');
+var menuBurger = document.querySelector('.header__burder-button');
+var filterButton = document.querySelector('.filters__filter-button');
+var popupFilter = document.querySelector('.popup-filter');
+
+filterButton.onclick = function () {
+  popupFilter.classList.toggle('popup-filter--active');
+
+  if (popupFilter.classList.contains('header--active')) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+};
 //# sourceMappingURL=index.js.map
