7b93bdae256a3229742a8b1c54ab747c129781d8
Nikita <dedsec1612@gmail.com>
2021-04-25 12:54:23 +0300

    JS + Map

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,44 @@
 'Use scrict'
 
+// Variables
 let menuBurger = document.querySelector('.header__burder-button');
 let filterButton = document.querySelector('.filters__filter-button');
 let popupFilter = document.querySelector('.popup-filter');
+let popupFilterButton = document.querySelector('.popup-filter__filter-button');
+let header = document.querySelector('.header');
+let mainMenu = document.querySelector('.header__main-menu');
+let headerMenuContacts = document.querySelector('.header__contacts');
+let burgerMenuIcon = document.querySelector('.header__burger-icon');
 
-filterButton.onclick = function() {
-  popupFilter.classList.toggle('popup-filter--active');
+//Menu burger
+menuBurger.onclick = function() {
+  header.classList.toggle('header--active');
+  mainMenu.classList.toggle('main-menu--active');
+  headerMenuContacts.classList.toggle('header__contacts--active');
+  burgerMenuIcon.classList.toggle('header__contacts--active');
 
-  if (popupFilter.classList.contains('header--active')) {
-    document.body.style.overflow = "hidden";
+  if (burgerMenuIcon.classList.contains('header__contacts--active')) {
+    burgerMenuIcon.src='/assets/images/burderCloseIcon.svg';
   } else {
+    burgerMenuIcon.src='assets/images/burger-icon.svg';
+  }
+}
+
+// Filters Popup
+filterButton.onclick = function() {
+  popupFilter.classList.add('popup-filter--active');
+
+  if (popupFilter.classList.contains('popup-filter--active')) {
+    document.body.style.overflow = "hidden";
+    // document.body.style.opacity = "20%";
+  }
+}
+
+popupFilterButton.onclick = function() {
+  popupFilter.classList.remove('popup-filter--active');
+
+  if (popupFilter.classList.contains('popup-filter--active') == false) {
     document.body.style.overflow = "visible";
+    // document.body.style.opacity = "100%";
   }
 }
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,17 +1,42 @@
 "use strict";
-'Use scrict';
+'Use scrict'; // Variables
 
 var menuBurger = document.querySelector('.header__burder-button');
 var filterButton = document.querySelector('.filters__filter-button');
 var popupFilter = document.querySelector('.popup-filter');
+var popupFilterButton = document.querySelector('.popup-filter__filter-button');
+var header = document.querySelector('.header');
+var mainMenu = document.querySelector('.header__main-menu');
+var headerMenuContacts = document.querySelector('.header__contacts');
+var burgerMenuIcon = document.querySelector('.header__burger-icon'); //Menu burger
+
+menuBurger.onclick = function () {
+  header.classList.toggle('header--active');
+  mainMenu.classList.toggle('main-menu--active');
+  headerMenuContacts.classList.toggle('header__contacts--active');
+  burgerMenuIcon.classList.toggle('header__contacts--active');
+
+  if (burgerMenuIcon.classList.contains('header__contacts--active')) {
+    burgerMenuIcon.src = '/assets/images/burderCloseIcon.svg';
+  } else {
+    burgerMenuIcon.src = 'assets/images/burger-icon.svg';
+  }
+}; // Filters Popup
+
 
 filterButton.onclick = function () {
-  popupFilter.classList.toggle('popup-filter--active');
+  popupFilter.classList.add('popup-filter--active');
 
-  if (popupFilter.classList.contains('header--active')) {
-    document.body.style.overflow = "hidden";
-  } else {
-    document.body.style.overflow = "visible";
+  if (popupFilter.classList.contains('popup-filter--active')) {
+    document.body.style.overflow = "hidden"; // document.body.style.opacity = "20%";
+  }
+};
+
+popupFilterButton.onclick = function () {
+  popupFilter.classList.remove('popup-filter--active');
+
+  if (popupFilter.classList.contains('popup-filter--active') == false) {
+    document.body.style.overflow = "visible"; // document.body.style.opacity = "100%";
   }
 };
 //# sourceMappingURL=index.js.map
