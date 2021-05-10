0bfbaef8731b04ff3b72b8eaea2b2d57013d4e0d
Nikita <dedsec1612@gmail.com>
2021-05-10 09:37:00 +0300

    Часть правок

--- app/js/index.js
+++ app/js/index.js
@@ -1,44 +1,40 @@
 'Use scrict'
 
 // Variables
-let menuBurger = document.querySelector('.header__burder-button');
+let menuBurger = document.querySelector('.header__burger-button');
 let filterButton = document.querySelector('.filters__filter-button');
 let popupFilter = document.querySelector('.popup-filter');
 let popupFilterButton = document.querySelector('.popup-filter__filter-button');
 let header = document.querySelector('.header');
 let mainMenu = document.querySelector('.header__main-menu');
 let headerMenuContacts = document.querySelector('.header__contacts');
-let burgerMenuIcon = document.querySelector('.header__burger-icon');
 
 //Menu burger
-menuBurger.onclick = function() {
+function menuBurgerFunction() {
   header.classList.toggle('header--active');
   mainMenu.classList.toggle('main-menu--active');
   headerMenuContacts.classList.toggle('header__contacts--active');
-  burgerMenuIcon.classList.toggle('header__contacts--active');
-
-  if (burgerMenuIcon.classList.contains('header__contacts--active')) {
-    burgerMenuIcon.src='/assets/images/burderCloseIcon.svg';
-  } else {
-    burgerMenuIcon.src='assets/images/burger-icon.svg';
-  }
+  menuBurger.classList.toggle('header__burger-button--active');
 }
 
+menuBurger.addEventListener('click', menuBurgerFunction);
+
 // Filters Popup
-filterButton.onclick = function() {
+function filterPopupAddFunction() {
   popupFilter.classList.add('popup-filter--active');
 
   if (popupFilter.classList.contains('popup-filter--active')) {
     document.body.style.overflow = "hidden";
-    // document.body.style.opacity = "20%";
   }
 }
 
-popupFilterButton.onclick = function() {
+function filterPopupRemoveFunction() {
   popupFilter.classList.remove('popup-filter--active');
 
   if (popupFilter.classList.contains('popup-filter--active') == false) {
     document.body.style.overflow = "visible";
-    // document.body.style.opacity = "100%";
   }
 }
+
+filterButton.addEventListener('click', filterPopupAddFunction);
+popupFilterButton.addEventListener('click', filterPopupRemoveFunction);
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,42 +1,39 @@
 "use strict";
 'Use scrict'; // Variables
 
-var menuBurger = document.querySelector('.header__burder-button');
+var menuBurger = document.querySelector('.header__burger-button');
 var filterButton = document.querySelector('.filters__filter-button');
 var popupFilter = document.querySelector('.popup-filter');
 var popupFilterButton = document.querySelector('.popup-filter__filter-button');
 var header = document.querySelector('.header');
 var mainMenu = document.querySelector('.header__main-menu');
-var headerMenuContacts = document.querySelector('.header__contacts');
-var burgerMenuIcon = document.querySelector('.header__burger-icon'); //Menu burger
+var headerMenuContacts = document.querySelector('.header__contacts'); //Menu burger
 
-menuBurger.onclick = function () {
+function menuBurgerFunction() {
   header.classList.toggle('header--active');
   mainMenu.classList.toggle('main-menu--active');
   headerMenuContacts.classList.toggle('header__contacts--active');
-  burgerMenuIcon.classList.toggle('header__contacts--active');
+  menuBurger.classList.toggle('header__burger-button--active');
+}
 
-  if (burgerMenuIcon.classList.contains('header__contacts--active')) {
-    burgerMenuIcon.src = '/assets/images/burderCloseIcon.svg';
-  } else {
-    burgerMenuIcon.src = 'assets/images/burger-icon.svg';
-  }
-}; // Filters Popup
+menuBurger.addEventListener('click', menuBurgerFunction); // Filters Popup
 
-
-filterButton.onclick = function () {
+function filterPopupAddFunction() {
   popupFilter.classList.add('popup-filter--active');
 
   if (popupFilter.classList.contains('popup-filter--active')) {
-    document.body.style.overflow = "hidden"; // document.body.style.opacity = "20%";
+    document.body.style.overflow = "hidden";
   }
-};
+}
 
-popupFilterButton.onclick = function () {
+function filterPopupRemoveFunction() {
   popupFilter.classList.remove('popup-filter--active');
 
   if (popupFilter.classList.contains('popup-filter--active') == false) {
-    document.body.style.overflow = "visible"; // document.body.style.opacity = "100%";
+    document.body.style.overflow = "visible";
   }
-};
+}
+
+filterButton.addEventListener('click', filterPopupAddFunction);
+popupFilterButton.addEventListener('click', filterPopupRemoveFunction);
 //# sourceMappingURL=index.js.map
